d9bda62d56cc127784bb8b54a08f7c98b59456f7
ForeverSc <fengshuochn@126.com>
2016-03-06 13:18:12 +0800

    Add content smooth scroll

--- source/js/framework.js
+++ source/js/framework.js
@@ -92,6 +92,16 @@ $(window).scroll(function () {
         });
     }
 });
+
+
+$(".jumptohead").click(function(){
+    $("html,body").animate({
+        scrollTop: 0
+    }, 800, 'swing');
+    return false;
+});
+
+
 //返回
 function goBack() {
     window.history.back();
